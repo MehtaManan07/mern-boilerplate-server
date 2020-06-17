f6cf600fc01b6fcf5a8038129536eda21712b63d
MehtaManan07 <f20190507@pilani.bits-pilani.ac.in>
2020-06-17 18:50:31 +0530

    Successfully sending messages

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,35 @@
-exports.signup = (req,res) => {
-    res.json({
-        msg: 'bhai sahab'
+const User = require("../models/User");
+const jwt = require("jsonwebtoken");
+const sgMail = require("@sendgrid/mail");
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+exports.signup = (req, res) => {
+  const { email, password, name } = req.body;
+  User.findOne({ email }).exec((err, user) => {
+    if (user) {
+      return res.json(400).json({
+        error: "Email is taken",
+      });
+    }
+    const token = jwt.sign(
+      { name, email, password },
+      process.env.JWT_ACCOUNT_ACTIVATION,
+      { expiresIn: "20m" }
+    );
+    const emailData = {
+        from: process.env.EMAIL_FROM,
+        to: email,
+        subject: `Account activation link`,
+        html: `
+        <p> Please use the following link to activate your account </p>
+        <p> ${process.env.CLIENT_URL}/auth/activate/${token} </p>
+        <hr />
+        <p> Ohh bhai, aavo aavo, welcome </p>
+        `
+    }
+    sgMail.send(emailData).then(sent => res.json({ message: `Sent successfully to ${email}. Follow the instructions to activate your account` })).catch((err) => {
+        console.log(err)
+        res.json({ err })
     })
-}
+  });
+};
