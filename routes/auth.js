72e3d827cf14982ba605e77b24b0970e9f426dfe
MehtaManan07 <f20190507@pilani.bits-pilani.ac.in>
2020-06-17 16:19:07 +0530

    Basic controllers

--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,7 @@
 const express = require('express')
+const { signup } = require('../controllers/auth')
 const router = express.Router()
 
-router.get('/', (req,res) => {
-    res.json({
-        msg: 'Successgul'
-    })
-})
+router.get('/', signup )
 
 module.exports = router
