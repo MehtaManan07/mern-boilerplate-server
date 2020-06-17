5f7b8fb19d487a3b4ab7397dd280eb5d3c3b6cdf
MehtaManan07 <f20190507@pilani.bits-pilani.ac.in>
2020-06-17 16:14:24 +0530

    Ultra basic routing

--- server.js
+++ server.js
@@ -2,11 +2,7 @@ const express = require('express');
 
 const app = express()
 
-app.get('/', (req,res) => {
-    res.json({
-        msg: 'Successgul'
-    })
-})
+app.use('/api/', require('./routes/auth'))
 
 const port = process.env.PORT || 5000
 
