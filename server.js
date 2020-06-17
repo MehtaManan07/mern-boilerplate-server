086cf87dc360a8e1002abf3ff1b63c6a40ad5e88
MehtaManan07 <f20190507@pilani.bits-pilani.ac.in>
2020-06-17 17:29:47 +0530

    Connected to atlas

--- server.js
+++ server.js
@@ -1,11 +1,37 @@
-const express = require('express');
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+require("dotenv").config();
 
-const app = express()
+const app = express();
 
-app.use('/api/', require('./routes/auth'))
-
-const port = process.env.PORT || 5000
-
-app.listen(port, () => {
-    console.log(`App is running on ${port}`);
+//connect to database 
+mongoose.connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useFindAndModify: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+}, () => {
+    console.log("\nconnected to DATABSE")
 })
+.catch(err => console.log("DB error:",err))
+// import routes
+const authRoutes = require("./routes/auth");
+
+// app middlewares
+app.use(morgan("dev"));
+app.use(bodyParser.json());
+// app.use(cors()); // allows all origins
+if ((process.env.NODE_ENV = "development")) {
+  app.use(cors({ origin: `http://localhost:3000` }));
+}
+
+// middleware
+app.use("/api", authRoutes);
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`API is running on port ${port}`);
+});
